a7c679e79a3cf7cd650f37f9752262913303926f
deceptionfalls <deceptionfalls@proton.me>
2025-08-29 21:14:49 -0300

    feat: implementaçao models.py

---
+++ backend/app/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Integer, String, Column, Boolean, Float, Time
+from database import Base
+
+
+class Ecoponto(Base):
+    __tablename__ = "tbEcoponto"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome_local = Column(String(100), nullable=False)
+    endereco = Column(String(200), nullable=False)
+    cidade = Column(String(50), nullable=False)
+    estado = Column(String(2), nullable=False)
+    telefone = Column(String(9), nullable=True)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    hora_aberto = Column(Time, nullable=True)
+    hora_fecha = Column(Time, nullable=True)
+    aceita_entulho = Column(Boolean, default=False, nullable=False)
+    aceita_construcao = Column(Boolean, default=False, nullable=False)
+    aceita_papel = Column(Boolean, default=False, nullable=False)
+    aceita_papelao = Column(Boolean, default=False, nullable=False)
+    aceita_vidro = Column(Boolean, default=False, nullable=False)
+    aceita_metal = Column(Boolean, default=False, nullable=False)
+    aceita_movel = Column(Boolean, default=False, nullable=False)
+    aceita_poda_arvore = Column(Boolean, default=False, nullable=False)
+    aceita_bateria = Column(Boolean, default=False, nullable=False)
+    aceita_monitor = Column(Boolean, default=False, nullable=False)
+    aceita_celular = Column(Boolean, default=False, nullable=False)
+    aceita_impressora = Column(Boolean, default=False, nullable=False)
