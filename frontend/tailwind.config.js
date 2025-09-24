6d2021c7050c0969dad2cc4b574c4d1d7800b9d5
Denis Farias <denis_farias2011@hotmail.com>
2025-09-24 01:22:44 -0300

    criar homepage

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,25 @@
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}", // for files in subfolders like /components
-        "./src/*.{js,ts,jsx,tsx}"    // for files in /src root
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        krona: ['Krona One', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      keyframes: {
+        'slide-in-left': {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-in-left': 'slide-in-left 1s ease-out forwards',
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [require('tailwindcss-animate')],
+};
